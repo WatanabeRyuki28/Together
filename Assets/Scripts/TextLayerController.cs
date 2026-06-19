c4bcad66dc0c58be2afa5b6b439b385f98a9080b
hayano <r06.gm.2140354@da.iwasaki.ac.jp>
2026-06-19 11:47:15 +0900

    ステージ4途中

--- Assets/Scripts/TextLayerController.cs
+++ Assets/Scripts/TextLayerController.cs
@@ -1,18 +1,22 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-
+ 
 public class TextLayerController : MonoBehaviour
 {
+    public string LayerName;
+    public int SortingOrder;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Order in Layer‚Ì”’l
+        this.GetComponent & lt; MeshRenderer > ().sortingOrder = SortingOrder;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
