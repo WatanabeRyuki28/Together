a8fa6e2c886ef1d33287f0957398033fa0756b81
hayano <r06.gm.2140354@da.iwasaki.ac.jp>
2026-06-22 15:41:45 +0900

    ボタンを押した時の変化を追加 プレイヤーの引っ掛かりを修正

--- Assets/Scripts/player/SimpleSwitch.cs
+++ Assets/Scripts/player/SimpleSwitch.cs
@@ -8,7 +8,13 @@ public class FloorSwitch : MonoBehaviour
     [SerializeField] private WarpDoor targetDoorB;
 
     [Header("スイッチの色設定")]
+    [SerializeField] private Color normalColor = Color.white;  // ★【追加】元の色（離れた時用）
     [SerializeField] private Color pressedColor = Color.gray;   // 踏まれた時の色
+
+    //[変更]　画像の設定を追加
+    [Header("スイッチの画像設定")]
+    [SerializeField] private Sprite normalSprite;  // 通常時（離れた時）の画像
+    [SerializeField] private Sprite pressedSprite; // 押された時の画像
 
     [Header("Audio Settings (効果音)")]
     [SerializeField] private AudioClip switchOnSound;  // スイッチが押された時の音
@@ -61,17 +67,20 @@ public class FloorSwitch : MonoBehaviour
         }
     }
 
-    // ★【状態切り替えの一括処理】
+    //[変更]　永久起動の処理
     private void SetSwitchState(bool isPressed)
     {
-        // スイッチの色を変更（踏まれて沈んだ状態のまま固定）
-
+        //スイッチの色を変更
         if (spriteRenderer != null)
-
         {
+            spriteRenderer.color = isPressed ? pressedColor : normalColor;
+        }
 
-            spriteRenderer.color = pressedColor;
-
+        //[追加]　スイッチの画像を差し替える処理
+        if (spriteRenderer != null)
+        {
+            //trueなら押された画像、falseなら通常時の画像にする
+            spriteRenderer.sprite = isPressed ? pressedSprite : normalSprite;
         }
 
         // ★スイッチが押された音を鳴らす
@@ -83,6 +92,7 @@ public class FloorSwitch : MonoBehaviour
             audioSource.PlayOneShot(switchOnSound);
 
         }
+
 
         // 3. 扉の状態を連動（trueなら開く、falseなら閉じる）
         if (targetDoorA != null) targetDoorA.SetDoorState(isPressed);
